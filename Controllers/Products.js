8236f0a1b03b7dc5a654cc7f24a1cdb7c134872c
Abraham <abrahamnhier97@gmail.com>
2024-02-01 20:27:06 +0300

    added update and delete productroutes and controllers

--- Controllers/Products.js
+++ Controllers/Products.js
@@ -1,5 +1,7 @@
+const CustomAPIError = require('../Errors/customeError')
 const Product = require('../Models/Products')
 const {StatusCodes} = require('http-status-codes')
+const path = require('path')
 
 const getAllProducts = async (req,res) => {
 // get all products  
@@ -26,10 +28,51 @@ const addProducts = async (req,res) => {
  res.status(StatusCodes.CREATED).json({ newProduct })  
 
 }
+const updateProducts = async (req,res) => {
+    const {id: productId} = req.params
+    const product = await Product.findOneAndUpdate({_id: productId}, req.body, {
+        new: true,
+        runValidators: true,
+    })
+    if (!product) {
+        throw new CustomAPIError(`no product witg id: ${productId}`)
+    }
+    res.status(StatusCodes.OK).json({ product })
+}
+const deleteProduct = async (req,res) => {
+    const { id: productId } = req.params;
+
+    const product = await Product.findOne({ _id: productId });
+  
+    if (!product) {
+      throw new CustomAPIError(`No product with id : ${productId}`);
+    }
+  
+    await product.remove();
+    res.status(StatusCodes.OK).json({ msg: 'Success! Product removed.' });
+}
+const uploadImage = async (req,res) => {
+    if (!req.files) {
+        throw new CustomAPIError("No files uploaded")
+    }
+    const productImage = req.files.uploadImage
+    if (!productImage.mimetype.startsWith('image')) {
+        throw new CustomAPIError('Please upload image')
+    }
+    const imagePath = path.join(
+        __dirname,
+        '../uploads' + `${productImage.name}`
+    )
+    await productImage.mv(imagePath);
+    res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` })
+}
 
 module.exports = {
     getAllProducts,
     getSingleProduct,
     addProducts,
+    updateProducts,
+    deleteProduct,
+    uploadImage,
 }
 
--- Routes/Products.js
+++ Routes/Products.js
@@ -15,11 +15,15 @@ const upload = multer({ storage: storage })
 const {
      getAllProducts,
       getSingleProduct, 
-      addProducts 
+      addProducts,
+      uploadImage,
+      updateProducts,
+      deleteProduct,
     } = require('../Controllers/Products')
 
 router.route('/').post(upload.single('image'),addProducts).get(getAllProducts)
+router.route('/uploadImage').post(uploadImage)
 
-router.route('/:id').get(getSingleProduct)
+router.route('/:id').get(getSingleProduct).patch(updateProducts).delete(deleteProduct)
 
 module.exports = router;
