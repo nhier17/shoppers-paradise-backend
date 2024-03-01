eea278aa5b55d07f4709ab790f7f28d09b6188e8
Abraham <abrahamnhier97@gmail.com>
2024-03-01 18:46:25 +0300

    express rate limit

--- app.js
+++ app.js
@@ -9,23 +9,31 @@ const cookieParser = require('cookie-parser');
 //security packages 
 const cors = require('cors')
 const helmet = require('helmet')
-
+const rateLimit = require('express-rate-limit')
+//db
 const connectDB = require('./db/connect-db')
-//error handler middleware
-const notFound = require('./Middleware/not-found')
-const errorHandlerMiddleware = require('./Middleware/error-handler')
 
 //routes
 const authRouter = require('./Routes/auth')
 const productRouter = require('./Routes/Products')
 const userRouter = require('./Routes/user')
 
-app.use('/uploads',express.static('uploads'))
+//error handler middleware
+const notFound = require('./Middleware/not-found')
+const errorHandlerMiddleware = require('./Middleware/error-handler')
+
+app.use(
+    rateLimit({
+        windowMs: 15 * 60 * 1000,
+        max: 100,
+    })
+)
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
 app.use(fileUpload())
 app.use(cookieParser(process.env.JWT_SECRET))
+app.use('/uploads',express.static('uploads'))
 
 
 app.get('/', (req, res) => {
